36fe96a1d1eb5eeb996d73b52229abc456579cd2
tao@gridx.com <tao@gridx.com>
2015-09-09 15:54:28 +0800

    ftp增加下载方法

--- examples/network/ftpClient/test.py
+++ examples/network/ftpClient/test.py
@@ -3,25 +3,41 @@ __author__ = 'tao'
 
 from ftplib import FTP
 import ftpClientUtils
+import os
 
 HOST = "fsnrec.com"
 USER = "xt"
 PASS = "Q592901703q"
 
+
 # 创建一个FTP实例并登录
 ftp = FTP(HOST)
 ftp.login(USER, PASS)
 
+
 # 获取预设的欢迎消息
 # 220 (vsFTPd 2.2.2)
-print ftp.getwelcome()
+#print ftp.getwelcome()
+
 # 进入目录『/0calibration』
 #ftp.cwd("/0calibration")
 
 # 列出该目录下的所有内容，以行的形式输出
-#ftp.retrlines('LIST')
+#ftp.retrlines('LIST LH004.pdf')
 
-# ftpClientUtils.downloadFile(ftp, "/0calibration/LH004.pdf", "/Users/tao/Downloads/ftp/LH004.pdf")
+#ftp.nlst()
 
-print ftpClientUtils.checkFileExists(ftp, "/lims/ArchiveReport/general/FZ20137001.pdf")
+# print ftpClientUtils.isDirectory(ftp, "/0calibration")
+
+# print ftpClientUtils.checkExisting(ftp, "/0calibratio")
+
+#ftpClientUtils.downloadFile(ftp, "/0calibration/LH004.pdf", "/Users/tao/Downloads/ftp/LH004.pdf")
+
+#print ftpClientUtils.checkFileExists(ftp, "/lims/ArchiveReport/general/FZ20137001.pdf")
+
+# ftpClientUtils.listOneFile(ftp, "/lims/ArchiveReport/general")
+
+ftpClientUtils.downloadDir(ftp, "/ganlan", "/Users/tao/code")
+
+#print ftpClientUtils.nlist(ftp, "/lims/ArchiveReport/general")
 
