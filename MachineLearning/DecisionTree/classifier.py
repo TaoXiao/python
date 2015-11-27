0fd422bea2ce4b414854be75bef8b893b87a0248
tao@gridx.com <tao@gridx.com>
2015-11-27 23:00:06 +0800

    增加决策树算法

---
+++ MachineLearning/DecisionTree/classifier.py
@@ -0,0 +1,35 @@
+# -*- encoding: utf-8 -*-
+__author__ = 'tao'
+
+import trees
+import json
+
+
+###################################################################
+# tree ： 构建出的决策树
+# featLabels : feature的名字的列表，例如 ["Survive", "Fish"]
+# testVec：一条测试数据，但是不知道其class
+###################################################################
+def classify(tree, featLabels, testVec):
+    firstStr = tree.keys()[0]
+    innerDict = tree[firstStr]
+    featIndex = featLabels.index(firstStr)
+    for key in innerDict.keys() :
+        if key == testVec[featIndex]:
+            if type(innerDict[key]).__name__ == 'dict':
+                return classify(innerDict[key], featLabels, testVec)
+            else:
+                return innerDict[key]
+
+
+
+# 测试新的数据的预测分类
+dataset, labels = trees.createDataset()
+decisionTree = trees.createDecisionTree(dataset, labels)
+# print decisionTree
+# print classify(decisionTree, ["Survive", "Fish"], [1, 0])
+
+
+dataset, labels = trees.generateDataSet()
+decisionTree = trees.createDecisionTree(dataset, labels)
+print classify(decisionTree, ['age', 'income', 'student', 'credit_rating'], ['senior',      'medium',   'no',   'excellent'])
--- MachineLearning/DecisionTree/trees.py
+++ MachineLearning/DecisionTree/trees.py
@@ -2,6 +2,15 @@
 __author__ = 'tao'
 
 from math import log
+import operator
+import json
+
+
+############################################################
+## 本例中的代码只能计算nominal类型的数据
+## 如果是连续性数据，则还要加以转换
+############################################################
+
 
 
 ############################################################
@@ -20,17 +29,16 @@ def calcShannonEntropy(dataset):
     datasetSize = len(dataset)
 
     # 计算每一种class的在数据集中出现的次数
-    for feature in dataset:
-        classTag = feature[-1]
+    for t in dataset:
+        classTag = t[-1]
         if classTag not in classCount.keys():
             classCount[classTag] = 0
         classCount[classTag] += 1
 
-    entropy = 0.0
-
     # 计算entropy
+    entropy = 0.0
     for classTag in classCount.keys():
-        prob = (float)(classCount[classTag])/datasetSize
+        prob = classCount[classTag]/float(datasetSize)
         entropy -= prob*log(prob, 2)
 
     return entropy
@@ -66,21 +74,24 @@ def splitDataset(dataset, splitIndex, value):
 ############################################################
 def chooseBestSplitCriteria(dataset):
     datasetSize = len(dataset)
-    entropy = calcShannonEntropy(dataset)
-    featNum = len(dataset[0])-1
-    bestCriteria = -1
+    baseEntropy = calcShannonEntropy(dataset) # 数据集D的熵
+    featNum = len(dataset[0])-1 # feature的数量（维度）
+    bestCriteria = -1 # 最佳split属性的index
     maxInfoGain = 0.0
 
     for i in range(featNum): # 针对每一维的feature来计算
-        featureValues = [t[0] for t in dataset]
+        featureValues = [t[i] for t in dataset]
         distinctFeatValues = set(featureValues)
         subEntropy = 0.0    # D在每一个属性处的entropy
         for v in distinctFeatValues: # 针对该feature的每一种值来计算
             subset = splitDataset(dataset, i, v)
             prob = len(subset)/float(datasetSize)
             subEntropy += prob * calcShannonEntropy(subset)
-        if entropy - subEntropy > maxInfoGain: # 寻找最大的信息增益
-            maxInfoGain = entropy - subEntropy
+            # 调试用：
+            # print "attribute index -> ", i, "\t subEntropy -> ", subEntropy,
+            # "\t info gain -> ", baseEntropy - subEntropy
+        if baseEntropy - subEntropy > maxInfoGain: # 寻找最大的信息增益
+            maxInfoGain = baseEntropy - subEntropy
             bestCriteria = i
 
     return bestCriteria
@@ -89,19 +100,102 @@ def chooseBestSplitCriteria(dataset):
 
 
 
+############################################################
+# 找出最主流的class(按照频率计算)
+############################################################
+def majorClass(classList):
+    classCountMap = {}
+    for clz in classList:
+        if clz not in classCountMap.keys():
+            classCountMap[clz] = 1
+        else:
+            classCountMap[clz] += 1
+    # 按照value进行排序，并返回频次最高的class的tag
+    return sorted(classCountMap.iteritems(), key=operator.itemgetter(1), reverse=True)[0][0]
 
 
 
 
+############################################################
+# 为一个数据集(nominal类型)构建一颗decision tree
+# attrLabels是所有attribues label的列表，其中的label的顺序
+# 与dataeset中每个tuple中的attribute value的顺序必须是一致的
+############################################################
+def createDecisionTree(dataset, featLabels):
+    classValueList = [t[-1] for t in dataset]  # 取出全部的class values
 
-dataset = [
-    [1, 1, 'yes'],
-    [1, 1, 'yes'],
-    [1, 0, 'no'],
-    [0, 1, 'no'],
-    [0, 1, 'no']
-]
+    # 如果dataset中全部的数据都属于同一个class，则返回该class
+    if classValueList.count(classValueList[0]) == len(classValueList):
+        return classValueList[0]
 
-# print(calcShannonEntropy(dataset))
+    # 如果dataset中已经没有attribute了，则返回所有class中的majority
+    if (len(dataset[0]) == 1):
+        return majorClass(classValueList)
 
-print chooseBestSplitCriteria(dataset)
+    # 找出目前dataset中最重要的feature的index
+    bestFeatIdx = chooseBestSplitCriteria(dataset)
+    bestFeatLabel = featLabels[bestFeatIdx]
+    subFeatLabels = featLabels[:]
+    tree = {bestFeatLabel:{}} # 构造根节点
+
+    # 下面处理根节点的每一个branch
+    del(subFeatLabels[bestFeatIdx])  # 去掉这个feature label
+    featValueSet = set([x[bestFeatIdx] for x in dataset])
+    for featValue in featValueSet:
+        tree[bestFeatLabel][featValue] = createDecisionTree(
+            splitDataset(dataset, bestFeatIdx, featValue),
+            subFeatLabels)
+
+    return tree
+
+
+
+
+############################################
+# 下面用数据来测试
+############################################
+def createDataset():
+    dataset = [
+        [1, 1, 'yes'],
+        [1, 1, 'yes'],
+        [1, 0, 'no'],
+        [0, 1, 'no'],
+        [0, 1, 'no']
+    ]
+    labels = ["Survive", "Fish"]
+    return dataset, labels
+
+
+def test1():
+    dataset, labels = createDataset()
+    print json.dumps(createDecisionTree(dataset, labels), indent=4)
+
+
+############################################################
+# 再用一组新数据来测试
+# 数据来源：《Data Mining: Concepts and Techniques》
+############################################################
+def generateDataSet():
+    dataset = [
+        ['youth',       'high',     'no',   'fair',         'no'],
+        ['youth',       'high',     'no',   'excellent',    'no'],
+        ['middle_aged', 'high',     'no',   'fair',         'yes'],
+        ['senior',      'medium',   'no',   'fair',         'yes'],
+        ['senior',      'low',      'yes',  'fair',         'yes'],
+        ['senior',      'low',      'yes',  'excellent',    'no'],
+        ['middle_aged', 'low',      'yes',  'excellent',    'yes'],
+        ['youth',       'medium',   'no',   'fair',         'no'],
+        ['youth',       'low',      'yes',  'fair',         'yes'],
+        ['senior',      'medium',   'yes',  'fair',         'yes'],
+        ['youth',       'medium',   'yes',  'excellent',    'yes'],
+        ['middle_aged', 'medium',   'no',   'excellent',    'yes'],
+        ['middle_aged', 'high',     'yes',  'fair',         'yes'],
+        ['senior',      'medium',   'no',   'excellent',    'no']
+    ]
+    labels = ['age', 'income', 'student', 'credit_rating', 'buys_computer']
+    return dataset, labels
+
+
+def test2():
+    dataset, labels = generateDataSet()
+    print json.dumps(createDecisionTree(dataset, labels), indent=4)
